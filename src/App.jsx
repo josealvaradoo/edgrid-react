66efaa65163a8affb91ee16a232ff5b7f30dc6da
Jose Alvarado <jose_alvarado17@hotmail.com>
2018-02-12 23:38:24 -0400

    Agrega un ejemplo de grilla utilizando los componentes de EDgrid

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,49 @@
 import React from 'react';
+import EDcontainer from './components/EDcontainer';
+import EDitem from './components/EDitem';
+import EDGridLogo from './components/EDGridLogo'
 
 const App = props => (
 	<React.Fragment>
+		<EDcontainer main="center" cross="center">
+			<EDitem main="center">
+				<EDGridLogo alt="EDgrid" size="200"/>
+			</EDitem>
+			<EDitem>&nbsp;</EDitem>
+			<EDitem m="25">
+					<h3>Hola mundo</h3>
+					<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati harum iusto facere assumenda dolores corrupti rerum animi cupiditate officiis, debitis in esse quisquam reprehenderit quo laborum illum libero maiores totam.</p>
+			</EDitem>
+			<EDitem m="25">
+					<h3>Hola mundo</h3>
+					<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati harum iusto facere assumenda dolores corrupti rerum animi cupiditate officiis, debitis in esse quisquam reprehenderit quo laborum illum libero maiores totam.</p>
+			</EDitem>
+			<EDitem m="25">
+					<h3>Hola mundo</h3>
+					<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati harum iusto facere assumenda dolores corrupti rerum animi cupiditate officiis, debitis in esse quisquam reprehenderit quo laborum illum libero maiores totam.</p>
+			</EDitem>
+			<EDitem m="25">
+					<h3>Hola mundo</h3>
+					<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati harum iusto facere assumenda dolores corrupti rerum animi cupiditate officiis, debitis in esse quisquam reprehenderit quo laborum illum libero maiores totam.</p>
+			</EDitem>
+			<EDitem m="25">
+					<h3>Hola mundo</h3>
+					<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati harum iusto facere assumenda dolores corrupti rerum animi cupiditate officiis, debitis in esse quisquam reprehenderit quo laborum illum libero maiores totam.</p>
+			</EDitem>
+			<EDitem m="25">
+					<h3>Hola mundo</h3>
+					<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati harum iusto facere assumenda dolores corrupti rerum animi cupiditate officiis, debitis in esse quisquam reprehenderit quo laborum illum libero maiores totam.</p>
+			</EDitem>
+			<EDitem m="25">
+					<h3>Hola mundo</h3>
+					<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati harum iusto facere assumenda dolores corrupti rerum animi cupiditate officiis, debitis in esse quisquam reprehenderit quo laborum illum libero maiores totam.</p>
+			</EDitem>
+			<EDitem m="25">
+					<h3>Hola mundo</h3>
+					<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati harum iusto facere assumenda dolores corrupti rerum animi cupiditate officiis, debitis in esse quisquam reprehenderit quo laborum illum libero maiores totam.</p>
+			</EDitem>
+		</EDcontainer>
 	</React.Fragment>
 )
 
-export const App;
+export default App;

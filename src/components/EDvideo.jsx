abfbcd124e4fe0c6036118cf51697ac6f2e2957d
Jose Alvarado <jose_alvarado17@hotmail.com>
2018-02-17 15:17:07 -0400

    :hammer: Reemplaza el PureComponent por Component

--- src/components/EDvideo.jsx
+++ src/components/EDvideo.jsx
@@ -1,6 +1,6 @@
-import React, {PureComponent} from 'react';
+import React, {Component} from 'react';
 
-export default class EDvideo extends PureComponent {
+export default class EDvideo extends Component {
 	render() {
 		return (
 			<div className={`ed-video ${(this.props.className) ? this.props.className : ``}`}>

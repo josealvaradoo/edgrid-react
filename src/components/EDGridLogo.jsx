bd341d2b17f1d0bce0dbd8016ba4e836a714a77c
Jose Alvarado <jose_alvarado17@hotmail.com>
2018-02-17 16:20:04 -0400

    :sparkles: Agrega atributo className al logo de EDgrid

--- src/components/EDGridLogo.jsx
+++ src/components/EDGridLogo.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import SVG from '../assets/images/EDgrid.svg'
 
 const EDGridLogo = props => (
-	<img src={SVG} alt={props.alt} width={props.size}/>
+	<img src={SVG} alt={props.alt} width={props.size} className={props.className}/>
 )
 
 export default EDGridLogo;

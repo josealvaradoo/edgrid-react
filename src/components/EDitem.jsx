0fdfbd78601ab9383e5f83dd8fdda6ad84f18e16
Jose Alvarado <jose_alvarado17@hotmail.com>
2018-02-17 16:14:56 -0400

    :art: Refactoriza el código de las clases de los componentes para ser más legible en desarrollo

--- src/components/EDitem.jsx
+++ src/components/EDitem.jsx
@@ -1,9 +1,54 @@
 import React, {Component} from 'react';
 
 export default class EDitem extends Component {
+	constructor(...props) {
+		super(...props);
+		this.state = {
+			className: ""
+		}
+		this.classes = this.classes.bind(this);
+	}
+
+	componentWillMount() {
+		this.classes();
+	}
+
+	classes() {
+		let className = "ed-item ";
+		className = (this.props.s) ? `${className}s-${this.props.s} ` : className
+		className = (this.props.m) ? `${className}m-${this.props.m} ` : className
+		className = (this.props.l) ? `${className}l-${this.props.l} ` : className
+		className = (this.props.xl) ? `${className}l-${this.props.xl} ` : className
+		className = (this.props.to) ? `${className}to-${this.props.to} ` : className
+		className = (this.props.from) ? `${className}from-${this.props.from} ` : className
+		className = (this.props.flex) ? `${className}flex-${this.props.flex} ` : className
+		className = (this.props.main) ? `${className}main-${this.props.main} ` : className
+		className = (this.props.cross) ? `${className}cross-${this.props.cross} ` : className
+		className = (this.props.sFlex) ? `${className}s-flex-${this.props.sFlex} ` : className
+		className = (this.props.mFlex) ? `${className}m-flex-${this.props.mFlex} ` : className
+		className = (this.props.lFlex) ? `${className}l-flex-${this.props.lFlex} ` : className
+		className = (this.props.xlFlex) ? `${className}xl-flex-${this.props.xlFlex} ` : className
+		className = (this.props.sMain) ? `${className}s-main-${this.props.sMain} ` : className
+		className = (this.props.mMain) ? `${className}m-main-${this.props.mMain} ` : className
+		className = (this.props.lMain) ? `${className}l-main-${this.props.lMain} ` : className
+		className = (this.props.xlMain) ? `${className}xl-main-${this.props.xlMain} ` : className
+		className = (this.props.sCross) ? `${className}s-cross-${this.props.sCross} ` : className
+		className = (this.props.mCross) ? `${className}m-cross-${this.props.mCross} ` : className
+		className = (this.props.lCross) ? `${className}l-cross-${this.props.lCross} ` : className
+		className = (this.props.xlCross) ? `${className}xl-cross-${this.props.xlCross} ` : className
+		className = (this.props.sOffset) ? `${className}s-offset-${this.props.sOffset} ` : className
+		className = (this.props.mOffset) ? `${className}m-offset-${this.props.mOffset} ` : className
+		className = (this.props.lOffset) ? `${className}l-offset-${this.props.lOffset} ` : className
+		className = (this.props.xlOffset) ? `${className}xl-offset-${this.props.xlOffset} ` : className
+		className = (this.props.padding) ? (this.props.padding === "0") ? `${className}no-padding ` : (this.props.padding === "1") ? `${className}padding ` : `${className}padding-${this.props.padding} ` : className
+		className = (this.props.className) ? `${className}${this.props.className}` : className
+
+		this.setState({ className })
+	}
+
 	render() {
 		return(
-			<div className={`ed-item ${(this.props.s) ? `s-${this.props.s} ` : ``}${(this.props.m) ? `m-${this.props.m} ` : ``}${(this.props.l) ?`l-${this.props.l} ` : ``}${(this.props.xl) ? `l-${this.props.xl} ` : ``}${(this.props.to) ? `to-${this.props.to} ` : ``}${(this.props.from) ? `from-${this.props.from} ` : ``}${(this.props.flex) ? `flex-${this.props.flex} ` : ``}${(this.props.main) ? `main-${this.props.main} ` : ``}${(this.props.cross) ? `cross-${this.props.cross} ` : ``}${(this.props.sFlex) ? `s-flex-${this.props.sFlex} ` : ``}${(this.props.mFlex) ? `m-flex-${this.props.mFlex} ` : ``}${(this.props.lFlex) ? `l-flex-${this.props.lFlex} ` : ``}${(this.props.xlFlex) ? `xl-flex-${this.props.xlFlex} ` : ``}${(this.props.sMain) ? `s-main-${this.props.sMain} ` : ``}${(this.props.mMain) ? `m-main-${this.props.mMain} ` : ``}${(this.props.lMain) ? `l-main-${this.props.lMain} ` : ``}${(this.props.xlMain) ? `xl-main-${this.props.xlMain} ` : ``}${(this.props.sCross) ? `s-cross-${this.props.sCross} ` : ``}${(this.props.mCross) ? `m-cross-${this.props.mCross} ` : ``}${(this.props.lCross) ? `l-cross-${this.props.lCross} ` : ``}${(this.props.xlCross) ? `xl-cross-${this.props.xlCross} ` : ``}${(this.props.sOffset) ? `s-offset-${this.props.sOffset} ` : ``}${(this.props.mOffset) ? `m-offset-${this.props.mOffset} ` : ``}${(this.props.lOffset) ? `l-offset-${this.props.lOffset} ` : ``}${(this.props.xlOffset) ? `xl-offset-${this.props.xlOffset} ` : ``}${(this.props.padding) ? ((this.props.padding === "0") ? 'no-padding ' : ((this.props.padding === "1") ? 'padding ' : `padding-${this.props.padding} `)) : ``}${(this.props.container) ? `ed-container ` : ``}${(this.props.className) ? this.props.className : ``}`}>
+			<div className={this.state.className}>
 				{this.props.children}
 				</div>
 		)
